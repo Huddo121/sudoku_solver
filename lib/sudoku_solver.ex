04f88c429a76b922fd1e1cb2ceb3b088e43fae62
Michael Hudson <huddo121@gmail.com>
2016-11-30 23:19:38 +1100

    Load and sanitise puzzles from files

--- lib/sudoku_solver.ex
+++ lib/sudoku_solver.ex
@@ -1,2 +1,14 @@
 defmodule SudokuSolver do
+    
+    def load_file(filename) do
+        {:ok, file} = File.read(filename)
+        puzzles = file 
+                    |> String.replace("|", "")
+                    |> String.replace("-", "")
+                    |> String.replace("0", ".")
+                    |> String.replace_trailing("\n", "")
+                    |> String.split("\n")
+
+    end
+
 end
