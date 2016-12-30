21616c7bfadf47f01a5c0f59bb9d73661349083a
Michael Hudson <huddo121@gmail.com>
2016-12-31 10:34:23 +1300

    clean up project directory slightly, updated README.

--- test/testing.exs
+++
@@ -1,5 +0,0 @@
-import SudokuSolver
-
-puzzle_text = "..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3.."
-{_, solution} = parse_grid(puzzle_text)
-IO.puts display(solution)
